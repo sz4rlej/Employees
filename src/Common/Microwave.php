52957ca8b64be2e7a5c7852479925aa4d9a73c35
sz4rlej <przemek@szarlej.pl>
2014-12-23 13:40:14 +0100

    Create Microwave.php

---
+++ src/Common/Microwave.php
@@ -0,0 +1,24 @@
+<?php
+namespace Employees\Common;
+
+class Microwave 
+{
+  private $content;
+  private $time;
+  private $temperature;
+  
+  public function putIn($content)
+  {
+    $this->content = $content;
+  }
+  
+  public function setTime($time)
+  {
+    $this->time = $time;
+  }
+  
+  public function setTemperature($temperature)
+  {
+    $this->temperature = $temperature;
+  }
+}
